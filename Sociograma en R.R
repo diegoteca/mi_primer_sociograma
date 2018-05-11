16f6a60e4f625c85c356ff5fd90a3677d78d95ba
diegoteca <dquartullidocencia@gmail.com>
2018-05-11 12:25:15 -0300

    primer push desde unaj

--- Sociograma en R.R
+++ Sociograma en R.R
@@ -12,19 +12,10 @@ library(tidygraph) # Packages for network analysis and visualization compatible 
 
 # Import data matrix in csv extension
 
-library(readr)
-mat_adyacencia <- read_delim("Bases/Bases Sincronizadas/mat_adyacencia.csv", 
-                             ";", escape_double = FALSE, trim_ws = TRUE)
-
-mat_adyacencia <- read.csv("~/Bases/Bases Sincronizadas/mat_adyacencia.csv", 
+mat_adyacencia <- read.csv("https://raw.githubusercontent.com/diegoteca/mi_primer_sociograma/master/mat_adyacencia.csv", 
                            row.names=1, 
                            sep=";",
                            header=TRUE)
-
-matrix_csv <-read.csv("Bases/Bases Sincronizadas/mat_adyacencia.csv",
-                      header=TRUE,
-                      row.names = 1)
-
 
 matrix <-as.matrix(mat_adyacencia)
 
