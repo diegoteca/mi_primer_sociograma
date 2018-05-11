8a4dcdeac5dcc095081eb97b86170897a1dd042e
diegoteca <dquartullidocencia@gmail.com>
2018-05-11 14:10:33 -0300

    Nuevos layouts

--- Sociograma en R.R
+++ Sociograma en R.R
@@ -10,6 +10,7 @@ library(readr)
 library(igraph) # Packages for network analysis and visualization
 library(tidygraph) # Packages for network analysis and visualization compatible with "tidy" data philosophy
 
+
 # Import data matrix in csv extension
 
 mat_adyacencia <- read.csv("https://raw.githubusercontent.com/diegoteca/mi_primer_sociograma/master/mat_adyacencia.csv", 
@@ -24,4 +25,21 @@ mi_primer_sociograma <- graph.adjacency(matrix,
                                         diag = FALSE)
 mi_primer_sociograma
 
-plot (mi_primer_sociograma)
+plot_reingold <-plot (mi_primer_sociograma, # Object of plot
+      main="Sociograma del curso", # Principal Title of plot
+      sub="Cada circulo representa una persona y cada línea representa la relación social de 'conocer a.", # Secundary tittle of plot
+      layout=layout.fruchterman.reingold) #Layout algorithm
+
+plot_circle <-plot (mi_primer_sociograma, # Object of plot
+              main="Sociograma del curso", # Principal Title of plot
+              sub="Cada circulo representa una persona y cada línea representa la relación social de 'conocer a.", # Secundary tittle of plot
+              layout=layout.circle) #Layout algorithm
+prueba <-alpha.centrality(mi_primer_sociograma,
+                 nodes = V(mi_primer_sociograma),
+                 alpha =1,
+                 loops = FALSE)
+
+write_graph(g, "/tmp/g.txt", "edgelist")
+
+write_graph(mi_primer_sociograma, "graphml")
+
