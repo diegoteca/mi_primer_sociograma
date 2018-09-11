97e54ec76ee5bb2ed44c3f9c89cd4870415d4e65
diegoteca <dquartullidocencia@gmail.com>
2018-09-11 14:35:17 -0300

    Se prueba agregar atributos a 2C_2018

---
+++ Sociograma 2C_2018.R
@@ -0,0 +1,60 @@
+#Script mi primer sociograma#
+
+install.packages("readr")
+install.packages("igraph")
+install.packages("tidygraph")
+
+# load packages for network exploration 
+
+library(readr)
+library(igraph) # Packages for network analysis and visualization
+library(tidygraph) # Packages for network analysis and visualization compatible with "tidy" data philosophy
+
+#En esta dirección hay muy buena información
+#https://github.com/jalapic/SNA_workshop
+
+# Import data matrix in csv extension
+
+mat_adyacencia <- read.csv("https://raw.githubusercontent.com/diegoteca/mi_primer_sociograma/master/Matriz%20adyacencia%202C_2018.csv", 
+                           row.names=1, 
+                           sep=",",
+                           header=TRUE)
+
+matrix <-as.matrix(mat_adyacencia)
+
+mi_primer_sociograma <- graph.adjacency(matrix,
+                                        mode = "directed",
+                                        diag = FALSE)
+mi_primer_sociograma
+
+
+plot_reingold <-plot (mi_primer_sociograma, # Object of plot
+                      main="Sociograma del curso", # Principal Title of plot
+                      sub="Cada circulo representa una persona y cada línea representa la relación social de 'conocer a.", # Secundary tittle of plot
+                      layout=layout.fruchterman.reingold, #Layout algorithm
+                      vertex.size=15) # Size of the vertex
+
+plot_circle <-plot (mi_primer_sociograma, # Object of plot
+                    main="Sociograma del curso", # Principal Title of plot
+                    sub="Cada circulo representa una persona y cada línea representa la relación social de 'conocer a.", # Secundary tittle of plot
+                    layout=layout.circle, #Layout algorithm
+                    vertex.size=15) # Size of the vertex
+
+#Some analytics about this social network#
+
+#Betweenness (Centralidad)#
+
+intermediacion <- betweenness(mi_primer_sociograma,
+                              directed = TRUE)
+
+summary(intermediacion)
+
+print(intermediacion)
+
+#Degree (Grado de centralidad)#
+
+grado <- degree(mi_primer_sociograma,
+                mode= "ALL")
+
+summary(grado)
+print(grado)
